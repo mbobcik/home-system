c85eb1f0c808f73d05df1f3e244516c25a0fd2fe
mbobcik <bobcik.martin1@gmail.com>
2023-08-05 23:39:00 +0200

    JobMaster src init

---
+++ src/Home-System/Home-System/Controllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Home_System.Controllers
+{
+    [ApiController]
+    [Route("job")]
+    public class JobController : Controller
+    {
+        private readonly ILogger<JobController> _logger;
+
+        public JobController(ILogger<JobController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpPost(Name = "start")]
+        public int StartJob() { 
+            return 0;
+        }
+
+
+    }
+}
