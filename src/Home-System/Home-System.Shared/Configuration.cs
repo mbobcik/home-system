39355cb555d392dde3554b3cc9a1920d42706841
mbobcik <bobcik.martin1@gmail.com>
2023-08-15 23:06:49 +0200

    Json Configuration Loader

---
+++ src/Home-System/Home-System.Shared/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace Home_System.Shared
+{
+    public class Configuration
+    {
+        public static string WorkDir { get; set; }
+
+        public Configuration() 
+        {
+            WorkDir = @"C:\Users\bobci\repo\home-system\workDir";
+        }
+    }
+}
---
+++ src/Home-System/Home-System.Shared/ConfigurationLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Home_System.Shared
+{
+    public class ConfigurationLoader
+    {
+        // TODO: Create as singleton. Or maybe somehow via dependency injection
+
+        private const string configFileName = "configuration.json";
+        private Configuration configuration;
+        private ConfigurationLoader() 
+        { 
+            if (File.Exists(configFileName))
+            {
+                string jsonString = File.ReadAllText(configFileName);
+                configuration = JsonSerializer.Deserialize<Configuration>(jsonString);
+                if (configuration == null )
+                {
+                    configuration = new Configuration();
+                }
+            }
+            else
+            {
+                configuration = new Configuration();
+                using FileStream createStream = File.Create(configFileName);
+                JsonSerializer.Serialize(createStream, configuration, new JsonSerializerOptions
+                {
+                    AllowTrailingCommas = true,
+                    WriteIndented = true,
+                });
+                createStream.Dispose();
+            }
+        }
+        
+    }
+}
